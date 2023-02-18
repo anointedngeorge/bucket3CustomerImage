2d9a6a490f823710dfcb1f66a97dd99fdb66777d
anointedngeorge <anointedngeorge@gmail.com>
2023-02-18 14:35:43 +0100

    casd3fddw

--- image/settings.py
+++ image/settings.py
@@ -73,13 +73,17 @@ WSGI_APPLICATION = 'image.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bucket3Image',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
