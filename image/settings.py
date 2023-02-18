281271c419435488c1a5de4fa8b5b873841716a0
anointedngeorge <anointedngeorge@gmail.com>
2023-02-18 14:10:38 +0100

    casd3fdc

--- image/settings.py
+++ image/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#wmbn=1%22wt(^r-^b7##(l39njj6re5^xtnxz+fuu*h@&%x&+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
